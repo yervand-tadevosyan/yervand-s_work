c072b74b5f130c06ee54a95e1bd217f395d9bb14
yervand-tadevosyan <yervand_tadevostan@edu.aua.am>
2021-05-07 13:18:33 +0400

    login and other ejs files added

--- server.js
+++ server.js
@@ -3,8 +3,35 @@ const app = express();
 
 const PORT = 5001;
 
+app.set("view engine", "ejs");
+
 app.get("/", (req,res) => {
-    res.send("Hello Class CSE120 ");
+    res.render("index");
+})
+
+app.get("/admin", (req, res) => {
+	res.render("admin");
+})
+
+app.get("/book", (req, res) => {
+	res.render("book");
+})
+
+app.get("/tenis", (req, res) => {
+	res.render("tennis");
+})
+
+app.get("/edit_book", (req, res) => {
+	res.render("edit_book");
+})
+
+app.get("/edit_tennis", (req, res) => {
+	res.render("edit_tennis");
+})
+
+
+app.get('/login', (req, res)=>{
+	res.render('login');
 })
 
 app.listen(PORT, () =>{
